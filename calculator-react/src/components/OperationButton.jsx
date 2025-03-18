7bf1a1f5b6d807eeeb085940d37bea4d2ce05ca3
Samedcan <samedcankurtar@gmail.com>
2025-03-18 21:38:03 +0100

    first commit

---
+++ calculator-react/src/components/OperationButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {ACTIONS} from '../App.jsx'
+
+
+function OperationButton({dispatch, operation}) {
+ return (
+    <button onClick={()=>dispatch({type: ACTIONS.CHOOSE_OPERATION, payload: {operation}})}>
+        {operation}
+    </button>
+ )
+}
+
+export default OperationButton
